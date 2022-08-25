0a7a1a70e3be1bba701246a23b71fe9e5f972f4a
Janice Chan <janice.chan1288@gmail.com>
2022-08-25 16:48:58 -0400

    add routes and switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import { Switch, Route } from 'react-router-dom'
 import {useState, useEffect} from 'react'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Navbar from './components/Navbar'
+
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const theme = createTheme({
   palette: {
@@ -57,8 +59,13 @@ function App() {
       <ThemeProvider theme={theme}> 
       <Navbar currentUser={currentUser} updateCurrentUser={updateCurrentUser} />  
       <Switch>  
-
-
+        <Route exact path="/rentals"><RentalsPage rentals={rentals} updateCurrentRental={updateCurrentRental} /></Route> 
+        <Route exact path="/rentals/new"><NewRentalsForm onAddRental={handleAddRental} currentUser={currentUser} /></Route> 
+        <Route exact path="/rentals/:id/edit"><UserRentalEdit currentUser={currentUser} onUpdateRental={handleEditRental} /></Route>
+        <Route exact path="/rentals/:id"><RentalsDetail rentalObj={currentRental} /></Route> 
+        <Route exact path="/users/:id"><UserPage currentUser={currentUser} rentals={userFilteredRentals} deleteRental={handleDeleteRental} /></Route>
+        <Route exact path="/login"><LoginForm updateCurrentUser={updateCurrentUser} /></Route>     
+        <Route exact path="/"><SignupForm updateCurrentUser={updateCurrentUser} /></Route> 
      </Switch> 
      </ThemeProvider>
     </div>
